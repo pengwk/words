d43d71b6db11ffa2f1a2e1d3676eafab943ab99e
peng weikang <pengwk2@gmail.com>
2017-05-01 15:53:55 +0800

    修复BUG，hasCaption数据类型错误，导致自动转换将所有"true" and "false" 都变成1。 学会了一行的if express if condition else another_express

---
+++ reporter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    This module does
+    Date created: 4/20/2016
+    Date last modified: 4/25/2016
+    Python Version: 2.7.10
+"""
+from __future__ import print_function
+from models import Video
+
+__author__ = "pengwk"
+__copyright__ = "Copyright 2016, pengwk"
+__credits__ = [""]
+__license__ = "Private"
+__version__ = "0.1"
+__maintainer__ = "pengwk"
+__email__ = "pengwk2@gmail.com"
+__status__ = "BraveHeart"
+
+
+def main():
+
+    return None
+
+
+def report_video_():
+
+    video_count = Video.query.count()
+    details_count = Video.query.filter(Video.title != None).count()
+    xml_count = Video.query.filter(Video.xml_transcript.startswith("<")).count()
+    none_transcript = Video.query.filter(Video.xml_transcript == None).count()
+
+    report_str = u"视频总数：{}\n已下载视频资料数：{}\n有字幕数：{}尚未下载字幕数：{}"
+    print(report_str.format(video_count, details_count, xml_count, none_transcript))
+    return None
+
+
+if __name__ == "__main__":
+    report_video_()
