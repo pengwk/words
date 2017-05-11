994af0156bab2fdfe23ff7527dbf6a9e8e7ad86a
peng weikang <pengwk2@gmail.com>
2017-05-11 17:37:58 +0800

    for pushing to server

---
+++ databse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    This module does
+    Date created: 4/20/2016
+    Date last modified: 4/25/2016
+    Python Version: 2.7.10
+"""
+
+__author__ = "pengwk"
+__copyright__ = "Copyright 2016, pengwk"
+__credits__ = [""]
+__license__ = "Private"
+__version__ = "0.1"
+__maintainer__ = "pengwk"
+__email__ = "pengwk2@gmail.com"
+__status__ = "BraveHeart"
+
+
+def main():
+    return None
+
+
+if __name__ == "__main__":
+    main()
+    
